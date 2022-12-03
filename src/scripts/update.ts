1369cfc8039615913f3695d3b0244a0a337a2716
Planet's Lightning Arrester <franciscogssm94@gmail.com>
2022-12-03 13:39:58 -0300

    fix update-assets script

--- src/scripts/update.ts
+++ src/scripts/update.ts
@@ -1,8 +1,11 @@
 import { AssetCrawler } from '../asset-crawler';
+import fs from 'fs';
+import path from 'path';
 
 async function main() {
   const crawler = new AssetCrawler();
   await crawler.getListedAssets();
+  fs.writeFileSync(path.join(__dirname, '..', '..', '..', 'assets.json'), JSON.stringify(crawler['assets']));
 }
 
 main();
