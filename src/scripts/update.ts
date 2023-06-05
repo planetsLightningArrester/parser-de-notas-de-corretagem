c6af2df448956bee56fd3819eea0aaded6c673b2
Planet's Lightning Arrester <franciscogssm94@gmail.com>
2023-06-04 22:50:10 -0300

    increase verbosity

--- src/scripts/update.ts
+++ src/scripts/update.ts
@@ -5,7 +5,7 @@ import _ from 'lodash';
 
 async function main() {
   const pathToWrite = path.join(__dirname, '..', '..', '..', 'assets.json');
-  const crawler = new AssetCrawler(false, 'minimal');
+  const crawler = new AssetCrawler(false, 'all');
   await crawler.fetchListedAssets();
   fs.writeFileSync(pathToWrite, JSON.stringify(_.sortBy(crawler['assets'], 'tradingName'), null, 2));
 }
